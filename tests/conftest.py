8f63442b4ee40f6d11a48d3fa06b4d324a5bc622
alex-pancho <pan.pan.4it@gmail.com>
2025-12-02 14:13:08 +0200

    more context here

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,41 @@
 import os
 import pytest
 
+from contextlib import contextmanager
 from framework.driver_factory import DriverFactory
 from framework.locator import DriverType
 from pages.login import LoginPage
 from pages.login_actions import LoginPageActions
 
 
-DEBUG_URL = os.getenv("DEBUG_URL", "http://localhost:5000/login/")
-TEST_URL = os.getenv("TEST_URL", "https://www.instagram.com/accounts/login/")
+DEBUG_URL = os.getenv("DEBUG_URL", "http://localhost:5000/")
+TEST_URL = os.getenv("TEST_URL", "https://www.instagram.com/")
 DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 HEADLESS = os.getenv("HEADLESS", "false").lower() == "true"
 
 
-@pytest.fixture(scope="session")
-def login_page():
-    """Fixture to provide LoginPageActions instance"""
-    url = DEBUG_URL if DEBUG else TEST_URL
+@contextmanager
+def build_context(url: str):
     context, browser, playwright = DriverFactory.create_playwright_local(
         browser_type="chromium", headless=HEADLESS
     )
     page = context.new_page()
 
     try:
-        # Navigate to login page
         page.goto(url)
-
-        # Create page object
-        l_page = LoginPage(page, DriverType.PLAYWRIGHT)
-        login_page = LoginPageActions(l_page)
-        yield login_page
+        yield page
     finally:
         context.close()
         browser.close()
         playwright.stop()
+
+
+@pytest.fixture(scope="session")
+def login_page():
+    """Fixture to provide LoginPageActions instance"""
+    url = DEBUG_URL if DEBUG else TEST_URL
+    # Create page object
+    with build_context(url) as page:
+        login_page = LoginPage(page, DriverType.PLAYWRIGHT)
+        login_page_actions = LoginPageActions(login_page)
+        yield login_page_actions
